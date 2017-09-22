baa7e23a937b3bb5a8b1dd8a80be927157551419
ufabc <daniel.escudero05@gmail.com>
2017-09-22 12:01:09 -0300

    Exercicio 01 da lista 01

---
+++ lista01/01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+	int valor;
+
+	printf ("Digite um valor: ");
+	scanf ("%d", &valor);
+
+	if (valor%2)
+		printf ("impar\n");
+	else
+		printf ("par\n");
+
+	return 0;
+}
