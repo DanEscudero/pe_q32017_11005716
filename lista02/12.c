676a61180a05a6f1fa0141198d7ba0e2915849ee
DanEscudero <32200114+DanEscudero@users.noreply.github.com>
2017-10-17 22:18:24 -0200

    nao terminado

---
+++ lista02/12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* FAZER */
+int coef_bin (int n, int k) {
+	
+}
+
+int main () {
+	int n, k, reultado;
+	
+	scanf ("%d %d", &n, &k);
+	resultado = coef_bin (n, k);
+	printf ("%d\n", resultado);	
+	
+	return 0;
+}
