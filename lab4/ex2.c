c62924d008ea931feaa160d46d7e04bde865701b
DanEscudero <daniel.escudero05@gmail.com>
2017-10-23 21:38:29 -0200

    lab4

--- lab4/ex2.c
+++ lab4/ex2.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define NUM 100
+#include <time.h>
+#define NUM 250000
 
 int primo (int n) {
 	int primo = 1;
-	if (n == 1) return 0;
+	if (n == 1 || !n) return 0;
 	for (int i = 2; i <= n/2 && primo == 1; i++) {
 		if (n%i == 0) primo = 0;
 	}
@@ -26,10 +27,26 @@ int soma_vet (char *v, int n) {
 	return soma;
 }
 
+void imprime_vet (char *v, int n) {
+	for (int i = 0; i < n; i ++)
+		printf ("%d ", v[i]);
+	printf ("\n\n");
+}
+
 int main () {
 	char *primos;
+	clock_t inicio, fim;
+	double tempo;
 	
+	printf ("na raça\n\n");
+	
+	inicio = clock();
 	primos = preenche_vetor (NUM);
-	printf ("%d\n", soma_vet (primos, NUM));
+	fim = clock();
+	tempo = ((double) (fim - inicio))/CLOCKS_PER_SEC;
+	
+	printf ("tempo: %.10lf\n\n", tempo);
+	printf ("soma: %d\n", soma_vet (primos, NUM));
+	
 	return 0;
 }
--- lab4/ex3.c
+++ lab4/ex3.c
@@ -1,1 +1,70 @@
-/* Dado uma arraty com todos os valores iguais a 1, começando da pos 2, podemos marcar todos os multiplos de 2 com 0. O proximo elemento igual a 1 sera o 3, repetimos com os multiplos de 3, e assim por diante. Nao e necessario fazer operacoes de divisao. Imprima a soma dos elementos desse array. compare os tempos de execucao */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define NUM 250000
+
+int primo (int n) {
+	int primo = 1;
+	if (n == 1 || !n) return 0;
+	for (int i = 2; i <= n/2 && primo == 1; i++) {
+		if (n%i == 0) primo = 0;
+	}
+	return primo;
+}
+
+char * preenche_vetor_2 (int n) {
+	char *v = malloc (sizeof (char) * n);
+	
+	v[0] = 0;
+	for (int i = 2; i < n; i ++) {
+		if (!v[i]){
+			v[i] = primo (i);
+			//marca multiplos como 0;
+			for (int j = 2*i; j < n; j += i)
+				v[j] = 0;
+		}
+	}
+	
+	return v;
+}
+
+void imprime_vet (char *v, int n) {
+	for (int i = 0; i < n; i ++)
+		printf ("%d ", v[i]);
+	printf ("\n\n");
+}
+
+int soma_vet (char *v, int n) {
+	int soma = 0;
+	for (int i = 0; i < n; i++)
+		soma += v[i];
+	return soma;
+}
+
+int main () {
+	char *primos;
+	clock_t inicio, fim;
+	double tempo;
+	
+	printf ("crivo\n\n");
+	
+	inicio = clock ();
+	primos = preenche_vetor_2 (NUM);
+	fim = clock ();
+	tempo = ((double) (fim - inicio))/CLOCKS_PER_SEC;
+	
+	printf ("tempo: %.10lf\n\n", tempo);
+	printf ("soma: %d\n", soma_vet (primos, NUM));
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
