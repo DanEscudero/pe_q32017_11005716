00c9d982356f22702f72be643099fb81f630c5a6
DanEscudero <daniel.escudero05@gmail.com>
2017-10-25 20:05:21 -0200

    PRONTO

--- lab2/area_circ.c
+++ lab2/area_circ.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
+#define MAX 1000000000
 
-float distancia_pontos (float x1, float y1, float x2, float y2) {
-	return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+double aleatorio()
+{
+  return rand() / (RAND_MAX + 1.0);
 }
 
-float aleatorio () {
-	return rand()/(double)(RAND_MAX + 1.0);
+double quad (double x) {
+	return x*x;
+}
+
+double quad_distancia (double x1, double y1, double x2, double y2) {
+	return quad(x2-x1) + quad(y2-y1);
+}
+
+double monte_carlo (int max) {
+	int dentro = 0;
+	double x, y;
+	
+	for (int i = 0; i < max; i ++){
+		x = aleatorio();
+		y = aleatorio();
+		if (quad_distancia (x, y, 0.5, 0.5) <= 0.25)
+			dentro ++;
+	}
+	return (4.0*dentro)/(1.0*max);
 }
 
 int main () {
-	int pts_totais, pts_dentro = 0, max;	
-	float x, y, pi;
+	int max;
 	srand (time(NULL));
 	
-	scanf ("%d", &max);
-	
-	while (pts_totais <= max) {
-		x = aleatorio();
-		y = aleatorio();
-		pts_totais ++;
-		
-		if (distancia_pontos (x, y, 0.5, 0.5) <= 0.5) {
-			pts_dentro ++;
-		}
-		
-	}
-	
-	pi = 4 * (pts_dentro/pts_totais);
-	
-	printf ("pi = %f\n", pi);
+	//scanf ("%d", &max);	
+	printf ("pi = %.20lf\n", monte_carlo(MAX));
 
 	return 0;
 }
